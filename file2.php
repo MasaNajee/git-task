38c8a43bec3aa0f0e7876f347807964ff4bdbe4c
MasaNajee <41231397+MasaNajee@users.noreply.github.com>
2018-10-09 14:01:17 +0300

    Update file2.php

--- file2.php
+++ file2.php
@@ -1,1 +1,12 @@
-testingdata2
+   Modified for KDiff3 by Joachim Eibl 2003, 2004, 2005.
+   The original file was part of GNU DIFF.
+   Copyright (C) 1988, 1989, 1992, 1993, 1994, 1995, 1998, 2001, 2002
+   Free Software Foundation, Inc.
+   GNU DIFF is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+   GNU DIFF is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
